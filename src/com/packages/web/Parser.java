c2a600be5b7ab9f6830354caaae62d1a919fd0b1
BangWooJord <58793833+BangWooJord@users.noreply.github.com>
2021-05-07 22:32:33 +0300

    Json fetch system working

--- src/com/packages/web/Parser.java
+++ src/com/packages/web/Parser.java
@@ -1,7 +1,26 @@
 package com.packages.web;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.*;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
 //M: This is a class that's gonna parse json
 public class Parser {
-
-
+    private static String readAll(Reader rd) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int cp;
+        while ((cp = rd.read()) != -1) {
+            sb.append((char) cp);
+        }
+        return sb.toString();
+    }
+    public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
+        try (InputStream is = new URL(url).openStream()) {
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
+            String jsonText = readAll(rd);
+            return new JSONObject(jsonText);
+        }
+    }
 }
