9ec23c0ed5988c3b165f1a2bb70cc380cf0bb04f
BangWooJord <58793833+BangWooJord@users.noreply.github.com>
2021-05-07 18:50:36 +0300

    Added a gui window, classes to request & parse json

---
+++ src/com/packages/gui/MainWindow.java
@@ -0,0 +1,29 @@
+package com.packages.gui;
+
+import com.packages.web.Request;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainWindow extends JFrame {
+    public MainWindow(String title, int w, int h){
+        //M: creating main window
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+            setSize(w, h);
+            setTitle(title);
+
+        //M: button that calls web visualise
+        JButton web_btn = new JButton("Fetch");
+            web_btn.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    Request.Fetch();
+                }
+            });
+            add(web_btn);
+
+        //M: Making the window visible
+        setVisible(true);
+    }
+}
--- src/com/packages/web/Request.java
+++ src/com/packages/web/Request.java
@@ -1,4 +1,8 @@
 package com.packages.web;
 
+//M: Class that's gonna fetch JSON from web
 public class Request {
+    public static void Fetch(){
+        System.out.println("Fetch() called!");
+    }
 }
